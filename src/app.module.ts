ee1dd2b03770a055381833d356094839ee56bb25
Rômulo Machado <tr041738@minhati.com.br>
2025-07-12 22:23:51 -0300

    chore: update app module and bootstrap to integrate new modules and global configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,25 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from './config/config.module';
+import { UserModule } from './user/user.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigService } from './config/config.service';
+import { SpotifyApiModule } from './spotify-api/spotify-api.module';
 
 @Module({
-  imports: [AuthModule, ConfigModule],
+  imports: [
+    ConfigModule,
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.envConfig.mongoUri,
+      }),
+    }),
+    AuthModule,
+    UserModule,
+    SpotifyApiModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
