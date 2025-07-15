bcf293137c8f8a29a989d4ef19c65dd75be7a8fd
Rômulo Machado <tr041738@minhati.com.br>
2025-07-15 16:55:31 -0300

    chore: added route logout

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { User } from 'src/user/schemas/user.schema';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly jwtService: JwtService) {}
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly userService: UserService,
+  ) {}
 
   generateJwt(user: User) {
     const payload = {
@@ -12,5 +16,12 @@ export class AuthService {
       spotifyId: user.spotifyId,
     };
     return this.jwtService.sign(payload);
+  }
+
+  async logout(userId: string): Promise<void> {
+    const user = await this.userService.findById(userId);
+    if (user) {
+      await this.userService.deleteUser(user.id);
+    }
   }
 }
